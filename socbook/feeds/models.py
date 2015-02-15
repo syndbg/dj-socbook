a5a8f73b576cc84a727617bba6d86c9e4f84e6b1
syndbg <syndbe@gmail.com>
2015-02-16 01:20:42 +0200

    Added Publiction visibility, PublicationLike and PublicationComment

--- socbook/feeds/models.py
+++ socbook/feeds/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 
 class Publication(models.Model):
+    PUBLIC, PRIVATE = range(2)
+    VISIBILITY_CHOICES = (
+        PUBLIC, 'Public',
+        PRIVATE, 'Private')
+
     author = models.ForeignKey('profiles.Profile', null=True)
     title = models.CharField(max_length=125, blank=False)
     content = models.TextField(max_length=500, blank=False)
+    visibility = models.SmallIntegerField(choices=VISIBILITY_CHOICES, default=PUBLIC)
 
-    class Meta:
-        unique_together = (('author', 'title'),)
+    @property
+    def likes_count(self):
+        return self.likes.count()
+
+    @property
+    def comments_count(self):
+        return self.comments.count()
 
 
 class PublicationComment(models.Model):
@@ -15,3 +26,8 @@ class PublicationComment(models.Model):
     content = models.TextField(max_length=500, blank=False)
     date_posted = models.DateTimeField(auto_now_add=True)
     publication = models.ForeignKey(Publication, related_name='comments')
+
+
+class PublicationLike(models.Model):
+    author = models.ForeignKey('profiles.Profile', null=True)
+    publication = models.ForeignKey(Publication, related_name='likes')
