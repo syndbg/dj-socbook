049da6ebc4069539c5ff01a7e1d913e4062cfec7
syndbg <syndbe@gmail.com>
2015-02-15 22:50:43 +0200

    Added Account.gender choices tests

--- socbook/accounts/tests/test_models.py
+++ socbook/accounts/tests/test_models.py
@@ -11,4 +11,9 @@ class AccountModelTests(TestCase):
     def test_instance(self):
         self.assertEqual('Anton', self.account.first_name)
         self.assertEqual('Antonov', self.account.last_name)
-        self.assertEqual('Male', self.account.gender)
+        self.assertEqual(Account.MALE, self.account.gender)
+
+    def test_default_instance_gender(self):
+        self.account.delete()
+        self.account = Account.objects.create(first_name='Anton', last_name='Antonov')
+        self.assertEqual(Account.SECRET, self.account.gender)
