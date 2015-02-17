0aceb23cbb4550330ecc2334bd60b25672a0fa2e
syndbg <syndbe@gmail.com>
2015-02-17 22:39:20 +0200

    Updated Accounts.signals

--- socbook/accounts/signals.py
+++ socbook/accounts/signals.py
@@ -2,7 +2,7 @@ from django.dispatch import Signal
 
 
 account_befriended = Signal(providing_args=['account', 'other_account'])
-friend_request_accepted = Signal(providing_args=[''])
-friend_request_rejected = Signal(providing_args=[])
-friend_request_sent = Signal(providing_args=['account', 'other_account'])
-new_profile_created = Signal(providing_args=['account', 'url'])
+friend_request_accepted = Signal(providing_args=['from_account', 'to_account'])
+friend_request_rejected = Signal(providing_args=['from_account', 'to_account'])
+friend_request_sent = Signal(providing_args=['from_account', 'to_account'])
+new_profile_created = Signal(providing_args=['account'])
