6b5b89f52c009268637553799a38503447ac8275
syndbg <syndbe@gmail.com>
2015-02-18 23:50:27 +0200

    Added very basic prototypes for views

--- socbook/accounts/views.py
+++ socbook/accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render, get_object_or_404
 
-# Create your views here.
+
+
+
+def signin(request):
+    if not request.user.is_authenticated():
+        return auth_views.login(request, template_name='signin.html')
+    return redirect('activities:activities')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def signout(request):
+    auth_views.logout(request)
+    return redirect('/')
+
+
+@login_required
+def settings(request):
+    pass
+
+
+@login_required
+def picture_settings(request):
+    pass
+
+
+@login_required
+def friend_settings(request):
+    pass
--- socbook/activities/views.py
+++ socbook/activities/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST, require_GET
 
-# Create your views here.
+from activities.models import Activity, Notification
+
+
+@login_required
+def notifications(request):
+    account = request.user
+    notifications = Notification.objects.filter(to_account=account)
+    return render(request, '', {'notifications': notifications})
+
+
+@require_POST
+@login_required
+def mark_seen_notification(request, notification_id):
+    notification = Notification.objects.filter(pk=notification_id).first()
+    if not notification:
+        return HttpResponse(status=404)
+
+    notification.seen = True
+    notification.save(update_fields=['seen'])
+    return HttpResponse(status=200)
+
