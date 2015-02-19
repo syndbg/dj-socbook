c4067d0977e9812e9f5e582eac0990a4400a39af
syndbg <syndbe@gmail.com>
2015-02-19 19:31:41 +0200

    Added desired Accounts.urls

--- socbook/accounts/urls.py
+++ socbook/accounts/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+
 
 urlpatterns = patterns('account.views',
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^signin/$', 'signin', name='signin'),
+                       url(r'^signout/$', 'signout', name='signout'),
+                       url(r'^settings/$', 'account_settings', name='settings'),
+                       url(r'^settings/account/$', 'account_settings', name='account_settings'),
+                       url(r'^settings/password/$', 'password_settings', name='password_settings'),
+                       url(r'^settings/picture/$', 'picture_settings', name='picture_settings'),
+                       url(r'^settings/friends/$', 'friends_settings', name='friends_settings'),
+                       url(r'^profiles/(?P<username>\w+)/$', 'profile', name='profile')
 )
