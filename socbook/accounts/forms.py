90d44d29c0671efa22bd952dd8181634725eb3fe
syndbg <syndbe@gmail.com>
2015-02-21 01:26:23 +0200

    Added Signup page. TODO alerts/errors in a fancy manner

--- socbook/accounts/forms.py
+++ socbook/accounts/forms.py
@@ -4,6 +4,31 @@ from accounts.models import Account
 
 
 class AccountSignupForm(forms.ModelForm):
+    gender = forms.ChoiceField(choices=Account.GENDERS, widget=forms.RadioSelect)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password Confirmation', widget=forms.PasswordInput,
+                                help_text='Enter the same password as above, for verification.')
+
+    error_messages = {
+        'password_mismatch': 'The two password fields didn\'t match.',
+    }
 
     class Meta:
         model = Account
+        fields = ['first_name', 'last_name', 'username', 'email', 'gender']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(self.error_messages['password_mismatch'],
+                                        code='password_mismatch',
+            )
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
